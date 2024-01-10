e2fb9ee4ed436a993e8e8827670e3f12084fb654
unknown <jritsqmet@gmail.com>
2024-01-10 08:50:04 -0500

    subir imagen desde la galeria

--- config/Config.tsx
+++ config/Config.tsx
@@ -1,7 +1,10 @@
 import { initializeApp } from "firebase/app";
-
 import { getAuth } from 'firebase/auth'
 import { getDatabase } from 'firebase/database'
+import { getStorage } from "firebase/storage";
+////////////////
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
   apiKey: "AIzaSyDH1pepWGShtkEHqoHP68WnGWP_1X8fT2E",
@@ -16,6 +19,12 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app)
+//export const auth = getAuth(app)
 export const db= getDatabase(app)
+export const storage = getStorage(app)
+////////////
+
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+});
 
--- screens/RecursosScreen.tsx
+++ screens/RecursosScreen.tsx
@@ -1,12 +1,78 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Button, StyleSheet, Text, View, Image } from 'react-native'
+import React, {useState} from 'react'
+
+/// IMAGE
+import * as ImagePicker from 'expo-image-picker';
+
+/// FIREBASE
+import { getStorage, ref, uploadBytes } from "firebase/storage";
+import { storage } from '../config/Config';
+
+/////
+import { LogBox } from "react-native"
+
+LogBox.ignoreAllLogs(true)
+
 
 export default function RecursosScreen() {
+
+  const [imagen, setImagen] = useState(' ')
+
+  ////// CARGAR UNA IMAGEN DESDE LA GALERIA
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImagen(result.assets[0].uri);
+    }
+  };
+
+  //// SUBIR UNA IMAGEN A FIREBASE STORAGE
+  async function subirImagen(nombre) {
+    const storageRef = ref(storage, 'usuarios/' + nombre);
+
+    try {
+        const response = await fetch(imagen);
+        const blob = await response.blob();
+
+        await uploadBytes(storageRef, blob, {
+            contentType: 'image/jpg'
+        });
+
+        console.log('La imagen se subió con éxito');
+
+        // Obtiene la URL de la imagen
+      //  const imageURL = await getDownloadURL(storageRef);
+        //console.log('URL de desacarga de la imagen', imageURL);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
   return (
     <View>
-      <Text>RecursosScreen</Text>
+      <Text>Subir imagen desde la galeria</Text>
+      <Button title='Seleccionar imagen' onPress={ () => pickImage() } />
+      <Image source={{ uri: imagen}} style={styles.img}/>
+
+      <Button title='cargar imagen' onPress={ ()=> subirImagen('avatar1')}/>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  img:{
+    width:300,
+    height:300,
+    resizeMode: 'contain'
+  }
+})
