c32be8095116d6a8354cc4a9ba47b806061b4105
jroserog <jroserog@itsqmet.edu.ec>
2024-01-16 07:04:14 -0500

    actualización

--- screens/WelcomeScreen.tsx
+++ screens/WelcomeScreen.tsx
@@ -1,10 +1,49 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Button } from 'react-native'
+import React, { useEffect, useState } from 'react'
 
-export default function WelcomeScreen() {
+import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
+import { auth } from '../config/Config';
+
+
+export default function WelcomeScreen( {navigation}:any ) {
+  const [acceso, setAcceso] = useState('')
+
+ function observable(){ 
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/auth.user
+        const uid = user.uid;
+        setAcceso(uid)
+        // ...
+      } else {
+        // User is signed out
+        navigation.navigate("Login")
+      }
+    });
+  }
+  
+
+  function compuesta(){
+    logout()
+    observable()
+  }
+  
+ 
+
+  function logout(){
+    signOut(auth).then(() => {
+      // Sign-out successful.
+    }).catch((error) => {
+      // An error happened.
+    });
+  }
+
   return (
     <View>
       <Text>WelcomeScreen</Text>
+
+      <Button title="logout" onPress={()=> compuesta()}/>
     </View>
   )
 }
